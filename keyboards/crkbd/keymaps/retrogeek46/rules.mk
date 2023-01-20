433c9a95c21ce1cac79f276028fb7e53a0aea2e9
retro <retro@Ubuntu.myguest.virtualbox.org>
2023-01-20 21:19:51 +0530

    Add my corne keymap

---
+++ keyboards/crkbd/keymaps/retrogeek46/rules.mk
@@ -0,0 +1,7 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+WPM_ENABLE      = yes
+RAW_ENABLE      = yes
+LTO_ENABLE      = yes
