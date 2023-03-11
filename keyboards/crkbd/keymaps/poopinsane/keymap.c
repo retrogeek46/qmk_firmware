46eddffc0d9c90cd0243d3a5e75d95e2e430b19f
retrogeek46 <divyangpradeep@gmail.com>
2023-03-11 23:25:34 +0530

    Add new keymap

--- keyboards/crkbd/keymaps/poopinsane/keymap.c
+++ keyboards/crkbd/keymaps/poopinsane/keymap.c
@@ -19,6 +19,28 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 #include "raw_hid.h"
+#include <print.h>
+
+#define L_ANGL S(KC_COMM)
+#define R_ANGL S(KC_DOT)
+
+int hid_code;
+int current_title_code[21];
+int current_artist_code[21];
+int time_code[21];
+bool media_updated;
+bool clock_updated;
+
+enum layers {
+    _BASE,
+    _SYMB,
+    _NUMP,
+    _MAPS,
+};
+
+enum custom_keycodes {
+    KC_OLED = SAFE_RANGE
+};
 
 enum oled_states {
     OLED_CLOCK,
@@ -27,18 +49,8 @@ enum oled_states {
     _OLED_STATE_RANGE
 };
 
-int hid_code;
-int current_title_code[21];
-int current_artist_code[21];
-int time_code[21];
-bool media_updated;
-bool clock_updated;
 uint8_t oled_state = OLED_CLOCK;
 bool reset_oled = false;
-
-enum custom_keycodes {
-    KC_OLED = SAFE_RANGE
-};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
@@ -49,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, FN_MO13,  KC_SPC,     KC_ENT, FN_MO23, KC_RALT
+                                          KC_LGUI,   MO(1),  KC_SPC,     KC_ENT,   MO(2), KC_RALT
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -62,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
+                                          KC_LGUI, _______,  KC_SPC,     KC_ENT,   MO(3), KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -70,17 +82,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
+      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_OLED,                      KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
+                                          KC_LGUI,   MO(3),  KC_SPC,     KC_ENT, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
 
   [3] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -99,6 +111,12 @@ const char code_to_name[60] = {
     'R', 'E', 'B', 'T',  '_', '-', '=', '[', ']', '\\',
     '#', ';', '\'', '`', ',', '.', '/', ':', ' ', ' '
 };
+
+void keyboard_post_init_user(void) {
+    // Customize these values to desired behaviour
+    debug_enable = true;
+    //debug_keyboard = true;
+}
 
 void send_keyboard_state(void) {
     uint8_t send_data[32] = {23};
@@ -168,91 +186,66 @@ void raw_hid_receive(uint8_t *data, uint8_t length) {
     }
 }
 
-
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_OLED:
+            if (record->event.pressed) {
+                if (oled_state == _OLED_STATE_RANGE - 1) {
+                    oled_state = 0;
+                } else {
+                    oled_state = oled_state + 1 % _OLED_STATE_RANGE;
+                }
+                reset_oled = true;
+                return false;
+            } else {
+                return true;
+            }
+        default:
+            return true;
+    }
+}
 
 #ifdef OLED_ENABLE
-#include <stdio.h>
-
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_keyboard_master()) {
-    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-  }
-  return rotation;
+    if (!is_keyboard_master()) {
+      return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+    }
+    // else {
+    //    return OLED_ROTATION_270; 
+    // }
+    return rotation;
 }
-
-#define L_BASE 0
-#define L_LOWER 2
-#define L_RAISE 4
-#define L_ADJUST 8
 
 void oled_render_layer_state(void) {
-    oled_write_P(PSTR("Layer: "), false);
-    switch (layer_state) {
-        case L_BASE:
-            oled_write_ln_P(PSTR("Default"), false);
+    switch (biton32(layer_state)) {
+        case _BASE:
+            oled_write_ln_P(PSTR("Layer: BASE"), false);
             break;
-        case L_LOWER:
-            oled_write_ln_P(PSTR("Lower"), false);
+        case _SYMB:
+            oled_write_ln_P(PSTR("Layer: SYMB"), false);
             break;
-        case L_RAISE:
-            oled_write_ln_P(PSTR("Raise"), false);
-            break;
-        case L_ADJUST:
-        case L_ADJUST|L_LOWER:
-        case L_ADJUST|L_RAISE:
-        case L_ADJUST|L_LOWER|L_RAISE:
-            oled_write_ln_P(PSTR("Adjust"), false);
+        case _NUMP:
+            oled_write_ln_P(PSTR("Layer: NUMP"), false);
             break;
     }
-}
-
-
-char keylog_str[24] = {};
-
-void set_keylog(uint16_t keycode, keyrecord_t *record) {
-  char name = ' ';
-    if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) ||
-        (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) { keycode = keycode & 0xFF; }
-  if (keycode < 60) {
-    name = code_to_name[keycode];
-  }
-
-  // update keylog
-  snprintf(keylog_str, sizeof(keylog_str), "%dx%d, k%2d : %c",
-           record->event.key.row, record->event.key.col,
-           keycode, name);
-}
-
-void oled_render_keylog(void) {
-    oled_write(keylog_str, false);
-}
-
-void render_bootmagic_status(bool status) {
-    /* Show Ctrl-Gui Swap options */
-    static const char PROGMEM logo[][2][3] = {
-        {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}},
-        {{0x95, 0x96, 0}, {0xb5, 0xb6, 0}},
-    };
-    if (status) {
-        oled_write_ln_P(logo[0][0], false);
-        oled_write_ln_P(logo[0][1], false);
-    } else {
-        oled_write_ln_P(logo[1][0], false);
-        oled_write_ln_P(logo[1][1], false);
-    }
-}
-
-void oled_render_logo(void) {
-    static const char PROGMEM crkbd_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
-        0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
-        0};
-    oled_write_P(crkbd_logo, false);
 }
 
 void oled_render_clock(void) {
+    // int clock_label_code[6] = {6, 15, 18, 6, 14, 0};
     bool skip = false;
+    // for (int i = 0; i < 6; i++) {
+    //     if (clock_label_code[i] == 0 || skip) {
+    //         skip = true;
+    //         oled_write_char(code_to_name[0], false);
+    //     } else {
+    //         oled_write_char(code_to_name[clock_label_code[i]], false);
+    //     }
+    // }
+
+    // oled_advance_page(false);
+
+    // int time_code[19] = {39, 32, 45, 9, 8, 5, 45, 31, 39, 31, 32, 1, 39, 34, 57, 39, 39, 19, 16};
+    skip = false;
     for (int i = 0; i < 19; i++) {
         if (time_code[i] == 0 || skip) {
             skip = true;
@@ -287,8 +280,11 @@ void oled_render_media(void) {
     }
 }
 
+// Used to draw on to the oled screen
 bool oled_task_user(void) {
-    if (is_keyboard_master()) {
+    if (!is_keyboard_master()) {
+        //
+    } else {
         if (reset_oled) {
             oled_clear();
             reset_oled = false;
@@ -302,38 +298,11 @@ bool oled_task_user(void) {
                 break;
             case OLED_LAYER:
                 oled_render_layer_state();
-                oled_render_keylog();
                 break;
             default:
                 oled_render_clock();
-                break;
         }
-    } else {
-        oled_render_logo();
     }
     return false;
 }
 #endif // OLED_ENABLE
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case KC_OLED:
-            if (record->event.pressed) {
-                if (oled_state == _OLED_STATE_RANGE - 1) {
-                    oled_state = 0;
-                } else {
-                    oled_state = oled_state + 1 % _OLED_STATE_RANGE;
-                }
-                reset_oled = true;
-                return false;
-            } else {
-                return true;
-            }
-        default:
-            return true;
-    }
-    if (record->event.pressed) {
-        set_keylog(keycode, record);
-    }
-    return true;
-}
