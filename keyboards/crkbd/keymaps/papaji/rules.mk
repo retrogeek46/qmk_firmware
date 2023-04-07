48476ad5f146b8ec23075ee6a2e2be660161ca26
retrogeek46 <divyangpradeep@gmail.com>
2023-04-07 23:44:38 +0530

    Add papaji keymap and other stuff

---
+++ keyboards/crkbd/keymaps/papaji/rules.mk
@@ -0,0 +1,7 @@
+MOUSEKEY_ENABLE = no     # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
