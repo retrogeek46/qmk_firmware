ebf99de16b7c6c301fba247ff6b22a572d102302
retrogeek46 <divyangpradeep@gmail.com>
2023-07-24 00:51:36 +0530

    Add vial keymap with oled and trackpad

---
+++ keyboards/crkbd/keymaps/vial/rules.mk
@@ -0,0 +1,16 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+LTO_ENABLE          = yes
+
+RGBLIGHT_ENABLE     = yes
+RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+MOUSEKEY_ENABLE     = yes
+OLED_ENABLE         = yes
+OLED_DRIVER         = SSD1306
+EXTRAKEY_ENABLE     = yes
+COMBO_ENABLE        = no
+
+QMK_SETTINGS        = no
+
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
